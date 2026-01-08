bb4085209fc9aaea8a766048c34755016ea47ca4
Aditya Kumar <37246449+roguezox@users.noreply.github.com>
2026-01-08 21:45:33 +0530

    fix: deployment issues

--- stream/src/main/java/com/example/stream/controller/EventRequest.java
+++
@@ -1,9 +0,0 @@
-package com.example.stream.controller;
-
-import lombok.Data;
-
-@Data
-public class EventRequest {
-    private String eventType;
-    private String payload;
-}
--- stream/src/main/java/com/example/stream/controller/MessageRequest.java
+++
@@ -1,9 +0,0 @@
-package com.example.stream.controller;
-
-import lombok.Data;
-
-@Data
-public class MessageRequest {
-    private String content;
-    private String sender;
-}
